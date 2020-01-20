8096bb4ed511a551bdda6c175694e4894909f1cb
qsaleh <qmsaleh@gmail.com>
2020-01-20 22:46:47 +0000

    Output table

--- express_server.js
+++ express_server.js
@@ -10,15 +10,15 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send('Hello!');
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  res.render("urls_index.ejs", {urls : urlDatabase});
 });
 
 app.listen(PORT, () => {
