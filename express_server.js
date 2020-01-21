0b3a5182c5c6283c183ff10484c7e9496e2a2095
qsaleh <qmsaleh@gmail.com>
2020-01-21 00:28:57 +0000

    created urls_new

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -17,8 +20,13 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
 
-app.get("/urls", (req, res) => {
-  res.render("urls_index.ejs", {urls : urlDatabase});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new",);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -29,3 +37,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+
+}
